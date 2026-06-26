6376dc6b95fd2f5e3c9c80080bdcf83ad2732a8b
corexaen <corexaen@gwnu.ac.kr>
2026-06-26 20:43:10 +0900

    test: button

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <myos>
-
+#include <unistd.h>
+#include <sched.h>
 int main() {
 	Ginfo screen;
 	get_ginfo(&screen);
-	Window* window = new Window({0,0,screen.width, screen.height},WINDOW_NOHEADER,WINDOW_EX_GUI);
+	Window* window = new Window({0,0,screen.width, screen.height},WINDOW_NOHEADER | WINDOW_NOBORDER,WINDOW_EX_GUI);
+	for (int y = 0; y < screen.height; y++) {
+		for (int x = 0; x < screen.width; x++) {
+			((uint32_t*)window->gbuf)[y * window->rect.width + x] = PIXEL_TRANSPARENT & 0xFFFFFF;
+		}
+	}
+	window->draw_frame(window->rect);
+	for (int y = 0; y < 100; y++) {
+		for (int x = 0; x < 100; x++) {
+			((uint32_t*)window->gbuf)[y * window->rect.width + x] = 0x00FF00FF;
+		}
+	}
+	window->draw_frame({ 0,0,100,100 });
+	while (true) {
+		wait_for_msg();
+		msg_t msg;
+		uint64_t result = receive_msg(&msg);
+		if (result == 0) {
+			if (fork() == 0) {
+				execv("@/myshell.o",nullptr);
+			}
+		}
+	}
 }
