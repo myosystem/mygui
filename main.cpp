7e5346f7f317287f601efb99f889db5e8c635c2d
corexaen <corexaen@gwnu.ac.kr>
2026-07-14 20:50:00 +0900

    fix: rezorder

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <myos>
 #include <unistd.h>
 #include <sched.h>
+#include <stdio.h>
 int main() {
 	Ginfo screen;
 	get_ginfo(&screen);
 	Window* window = new Window({0,0,screen.width, screen.height},WINDOW_NOHEADER | WINDOW_NOBORDER,WINDOW_EX_GUI);
 	for (int y = 0; y < screen.height; y++) {
 		for (int x = 0; x < screen.width; x++) {
-			((uint32_t*)window->gbuf)[y * window->rect.width + x] = PIXEL_TRANSPARENT & 0xFFFFFF;
+			((uint32_t*)window->gbuf)[y * window->rect.width + x] = PIXEL_TRANSPARENT;
 		}
 	}
 	window->draw_frame(window->rect);
@@ -17,13 +18,17 @@ int main() {
 		}
 	}
 	window->draw_frame({ 0,0,100,100 });
+	window->rezorder();
 	while (true) {
 		wait_for_msg();
 		msg_t msg;
 		uint64_t result = receive_msg(&msg);
 		if (result == 0) {
-			if (fork() == 0) {
-				execv("@/myshell.o",nullptr);
+			printf("mygui got msg type=%x\n", msg.type);
+			if (msg.type == MSG_MOUSE_LCLICK) {
+				if (fork() == 0) {
+					execv("@/myterminal.o", nullptr);
+				}
 			}
 		}
 	}
